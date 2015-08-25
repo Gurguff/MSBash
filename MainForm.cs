e2614c0f5d18c0f40e0ae486b86ba08d32e61df7
Jonas Schultz <jonas.schultz@gmail.com>
2015-08-25 19:11:16 +0200

    Updated to speedier code

--- MainForm.cs
+++ MainForm.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -121,7 +122,7 @@ namespace MSBash
 			
 			basetime = (Int64) ((3600000*ts.Hours)+(60000*ts.Minutes)+(1000*ts.Seconds)+ts.Milliseconds);
 			
-			
+			int limit = 16;
 			Random rnd = new Random();
 			
 			List<int> xs = new List<int>();
@@ -129,7 +130,7 @@ namespace MSBash
 			
 			Log( "Generating data" );
 			
-			for(int i = 0; i<500; i++)
+			for(int i = 0; i<limit; i++)
 			{
 				xs.Add(rnd.Next(250,500));
 				ys.Add(rnd.Next(250,500));       
@@ -138,21 +139,35 @@ namespace MSBash
 			Log( "Measuring..." );
 			
 			Stopwatch sw = new Stopwatch();						
-			
 			List<uint> pxs;
+
+			//Version 1
+			
+			pxs = new List<uint>();
+			sw.Reset();
+			sw.Start();
+			for (int i=0; i<limit; i++)
+				pxs.Add(WindowHelper.GetPixelColor( xs[i], ys[i] ));
+			sw.Stop();
+			Log( String.Format("Time Version 1: {0}", sw.Elapsed ));
+			
+			
+			//Version 2 [500 ~8s 1000 ~16s] 
+			/*
+ 			sw.Reset();
 			sw.Start();
 				pxs = WindowHelper.GetListPixelColors( xs, ys );
 			sw.Stop();
-			Log( String.Format("List Version: {0}", sw.Elapsed ));
+			Log( String.Format("Time Version 2: {0}", sw.Elapsed ));
+			*/
 			
-			pxs = new List<uint>();
-			
+			//Version 3 [500 ~0.05s 1000 ~0.05s 4000 ~0.05s] :)
 			sw.Reset();
 			sw.Start();
-			for (int i=0; i<500; i++)
-				pxs.Add(WindowHelper.GetPixelColor( xs[i], ys[i] ));
+				pxs = WindowHelper.GetSCListPixelColors( xs, ys );
 			sw.Stop();
-			Log( String.Format("Other Version: {0}", sw.Elapsed ));
+			Log( String.Format("Time Version 3: {0}", sw.Elapsed ));
+
 			
 			
 
@@ -383,6 +398,22 @@ namespace MSBash
 	        	pix.Add(GetPixel(hdc, xs[i], ys[i]));
 	        return pix;
 		}
+		
+		static public List<uint> GetSCListPixelColors(List<int> xs, List<int> ys)
+		{
+			ScreenCapture sc = new ScreenCapture();
+			// capture the screen and stores it in a image
+			Image img = sc.CaptureScreen();
+			Bitmap bmp = new Bitmap(img);
+	        List<uint> pix = new List<uint>();
+	        for (int i=0; i<xs.Count; i++)
+	        {
+	        	System.Drawing.Color cl = bmp.GetPixel(xs[i],ys[i]);
+	        	pix.Add((uint)cl.ToArgb());
+	        }
+	        return pix;
+			
+		}
 	        
 		static public bool ActivateWoW()
 		{
@@ -392,4 +423,122 @@ namespace MSBash
 		}
 
 	}
+    /// <summary>
+    /// Provides functions to capture the entire screen, 
+    /// or a particular window, and save it to a file.
+    /// </summary>
+    public class ScreenCapture
+    {
+        /// <summary>
+        /// Creates an Image object containing a screen shot of the entire desktop
+        /// </summary>
+        /// <returns></returns>
+        public Image CaptureScreen()
+        {
+            return CaptureWindow( User32.GetDesktopWindow() );
+        }
+        /// <summary>
+        /// Creates an Image object containing a screen shot of a specific window
+        /// </summary>
+        /// <param name="handle">The handle to the window. 
+        /// (In windows forms, this is obtained by the Handle property)</param>
+        /// <returns></returns>
+        public Image CaptureWindow(IntPtr handle)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle,ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = windowRect.bottom - windowRect.top;
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc,width,height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest,hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest,0,0,width,height,hdcSrc,0,0,GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest,hOld);
+            // clean up
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle,hdcSrc);
+            // get a .NET image object for it
+            Image img = Image.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+            return img;
+        }
+        /// <summary>
+        /// Captures a screen shot of a specific window, and saves it to a file
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+        public void CaptureWindowToFile(IntPtr handle, string filename, ImageFormat format)
+        {
+            Image img = CaptureWindow(handle);
+            img.Save(filename,format);
+        }
+        /// <summary>
+        /// Captures a screen shot of the entire desktop, and saves it to a file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+        public void CaptureScreenToFile(string filename, ImageFormat format)
+        {
+            Image img = CaptureScreen();
+            img.Save(filename,format);
+        }
+
+        /// <summary>
+        /// Helper class containing Gdi32 API functions
+        /// </summary>
+        private class GDI32
+        {
+
+            public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject,int nXDest,int nYDest,
+                int nWidth,int nHeight,IntPtr hObjectSource,
+                int nXSrc,int nYSrc,int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC,int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC,IntPtr hObject);
+        }
+
+        /// <summary>
+        /// Helper class containing User32 API functions
+        /// </summary>
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd,IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd,ref RECT rect);
+        }
+    }
 }
